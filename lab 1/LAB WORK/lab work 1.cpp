d9dad407e07928ef59299a8579e7036b2c15ce2e
Sohaibkhan818 <sohaibkhann818@gmail.com>
2026-03-10 21:57:33 +0500

    Add files via upload

---
+++ lab 1/LAB WORK/lab work 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>   // input output library include karte hain
+
+using namespace std;  // std namespace use karne ke liye
+
+int main()   // main function jahan se program start hota hai
+{
+    cout << "Hello World";   // screen par Hello World print karega
+
+    return 0;   // program successfully khatam hone ka signal
+}
